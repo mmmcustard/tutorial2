e475f565c9b864332bb9b1eec3ffe24d23fe8ac8
jamesnz007 <evans.james.edward@gmail.com>
2018-08-10 14:19:53 +1200

    removed main method

--- Greeter.java
+++ Greeter.java
@@ -17,14 +17,4 @@ public class Greeter {
     return messages.getString("greeting") + ", " + "_"+name.toUpperCase()+"_";
   }
 
-  public static void main(String[] args) {
-    if (args.length < 3) {
-      System.exit(1);
-    }
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
-  }
 }
